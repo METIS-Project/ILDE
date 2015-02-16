409bbfa425e0d8ec7fcb01b6d7b6deaeec6f7c72
Javier Hoyos <javierht@gmail.com>
2015-02-16 09:23:10 +0100

    Adding the AGPL license headers to the Basic LTI Adapter

--- Glue/ImplementationAdapters/BasicLTIAdapter/bin/install-basiclti.bat
+++ Glue/ImplementationAdapters/BasicLTIAdapter/bin/install-basiclti.bat
@@ -1,3 +1,24 @@
+:: *******************************************************************************
+:: Copyright (C) 2015 Intelligent & Cooperative Systems Research Group/Education,
+:: Media, Computing & Culture (GSIC-EMIC). University of Valladolid(UVA). 
+:: Valladolid, Spain. https://www.gsic.uva.es/
+:: 
+:: This file is part of Basic LTI Adapter.
+:: 
+:: Basic LTI Adapter is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU Affero General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or 
+:: (at your option) any later version.
+:: 
+:: Basic LTI Adapter is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+:: GNU Affero General Public License for more details.
+:: 
+:: You should have received a copy of the GNU Affero General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+:: ******************************************************************************
+
 :: Registers BasicLTIAdapter as a Windows service
 
 @ECHO ** Running install-basiclti...
