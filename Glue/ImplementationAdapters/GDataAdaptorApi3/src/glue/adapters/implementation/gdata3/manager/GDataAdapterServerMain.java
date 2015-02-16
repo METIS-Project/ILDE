4e4a2829a70035fe34bae333a4fc79be1863a8e8
Javier Hoyos <javierht@gmail.com>
2015-02-16 10:33:36 +0100

    Adding the AGPL headers to the GData adapter

--- Glue/ImplementationAdapters/GDataAdaptorApi3/src/glue/adapters/implementation/gdata3/manager/GDataAdapterServerMain.java
+++ Glue/ImplementationAdapters/GDataAdaptorApi3/src/glue/adapters/implementation/gdata3/manager/GDataAdapterServerMain.java
@@ -1,40 +1,24 @@
-/**
- This file is part of GDataAdapterApi3.
+/*******************************************************************************
+ * Copyright (C) 2015 Intelligent & Cooperative Systems Research Group/Education,
+ * Media, Computing & Culture (GSIC-EMIC). University of Valladolid(UVA). 
+ * Valladolid, Spain. https://www.gsic.uva.es/
+ * 
+ * This file is part of GData Adapter.
+ * 
+ * GData Adapter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * GData Adapter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
- GDataAdapterApi3 is a property of the Intelligent & Cooperative Systems
- Research Group (GSIC) from the University of Valladolid (UVA).
-
- Copyright 2011 GSIC (UVA).
-
- GDataAdapterApi3 is licensed under the GNU General Public License (GPL)
- EXCLUSIVELY FOR NON-COMMERCIAL USES. Please, note this is an additional
- restriction to the terms of GPL that must be kept in any redistribution of
- the original code or any derivative work by third parties.
-
- If you intend to use GDataAdapterApi3 for any commercial purpose you can
- contact to GSIC to obtain a commercial license at <glue@gsic.tel.uva.es>.
-
- If you have licensed this product under a commercial license from GSIC,
- please see the file LICENSE.txt included.
-
- The next copying permission statement (between square brackets []) is
- applicable only when GPL is suitable, this is, when GDataAdapterApi3 is
- used and/or distributed FOR NON COMMERCIAL USES.
-
- [ You can redistribute GDataAdapterApi3 and/or modify it under the
-   terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>
- ]
-*/
 package glue.adapters.implementation.gdata3.manager;
 
 
