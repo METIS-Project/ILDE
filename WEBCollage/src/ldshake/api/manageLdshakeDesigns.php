a6f5e4765097bbcb9627ab5a88477bbdae4144d1
Javier Hoyos <javierht@gmail.com>
2015-02-16 14:09:02 +0100

    Adding the AGPL license header to the Web Collage source code

--- WEBCollage/src/ldshake/api/manageLdshakeDesigns.php
+++ WEBCollage/src/ldshake/api/manageLdshakeDesigns.php
@@ -1,5 +1,24 @@
 <?php
 
+/*Copyright (C) 2015 Intelligent & Cooperative Systems Research Group/Education,
+Media, Computing & Culture (GSIC-EMIC). University of Valladolid(UVA). 
+Valladolid, Spain. https://www.gsic.uva.es/
+
+This file is part of Web Collage.
+
+Web Collage is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+Web Collage is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
 /**
  *  Functions to manage the ldshake designs in webcollage
  *  @author Javier E. Hoyos Torio 
